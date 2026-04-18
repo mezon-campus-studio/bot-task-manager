4349940d8ee1c8eba3ff398bb2b957ed29bdf85e
VoDaiLocz <locv2659@gmail.com>
2026-04-18 19:40:13 +0700

    feat: implement task service for MEZON-234

--- src/modules/task/task.service.ts
+++ src/modules/task/task.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CRUDService } from '@src/common/utils/crud';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
 import TaskEntity from './task.entity';
 
 @Injectable()
@@ -13,9 +15,47 @@ export class TaskService extends CRUDService<TaskEntity> {
     super(taskRepository);
   }
 
-  async createEntry(task: Partial<TaskEntity>): Promise<TaskEntity> {
+  private async create(task: Partial<TaskEntity>) {
     const newTask = this.taskRepository.create(task);
     return this.taskRepository.save(newTask);
+  }
+
+  async createTask(payload: CreateTaskDto): Promise<TaskEntity> {
+    return this.create({
+      ...payload,
+      code: payload.code || `TASK-${Date.now()}`,
+    });
+  }
+
+  async updateTask(
+    id: string,
+    payload: UpdateTaskDto,
+  ): Promise<TaskEntity | null> {
+    const existingTask = await this.findById(id);
+
+    if (!existingTask) {
+      return null;
+    }
+
+    return this.taskRepository.save({
+      ...existingTask,
+      ...payload,
+    });
+  }
+
+  async deleteTask(id: string): Promise<boolean> {
+    const existingTask = await this.findById(id);
+
+    if (!existingTask) {
+      return false;
+    }
+
+    await this.taskRepository.softRemove(existingTask);
+    return true;
+  }
+
+  async getTaskById(id: string): Promise<TaskEntity | null> {
+    return this.findById(id);
   }
 
   async findByCode(code: string): Promise<TaskEntity | null> {
