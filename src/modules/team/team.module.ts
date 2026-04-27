a3117809a2349a88c3e494018204bd00349ac8cb
HoangBao <vohoangbaoqn123@gmail.com>
2026-04-27 11:46:07 +0700

    config team.module, seeder.service, database.service for team module

--- src/modules/team/team.module.ts
+++ src/modules/team/team.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import TeamEntity from '@src/modules/team/team.entity';
+import { TeamController } from './team.controller';
 import { TeamService } from './team.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([TeamEntity])],
+  controllers: [TeamController],
   providers: [TeamService],
   exports: [TeamService],
 })
