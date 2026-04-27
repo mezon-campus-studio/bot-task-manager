3123c8646d913d3c70278bad36e66413c1c615ce
HoangBao <vohoangbaoqn123@gmail.com>
2026-04-27 09:17:12 +0700

    .

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import UserEntity from '@src/modules/user/user.entity';
+import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity])],
+  controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
 })
