7613f6a3150d37c86e6327eed9d35189742b4658
VoDaiLocz <locv2659@gmail.com>
2026-05-09 19:42:09 +0700

    [MEZON-162] feature add project context api

--- src/modules/project/project.module.ts
+++ src/modules/project/project.module.ts
@@ -1,14 +1,25 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProjectMemberModule } from '@src/modules/project-member/project-member.module';
+import { UserModule } from '@src/modules/user/user.module';
+import { ProjectContextService } from './project-context.service';
 import { ProjectOnboardingService } from './project-onboarding.service';
 import ProjectEntity from './project.entity';
 import { ProjectService } from './project.service';
 import { ProjectV1Controller } from './project.v1.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ProjectEntity])],
+  imports: [
+    TypeOrmModule.forFeature([ProjectEntity]),
+    ProjectMemberModule,
+    UserModule,
+  ],
   controllers: [ProjectV1Controller],
-  providers: [ProjectOnboardingService, ProjectService],
-  exports: [ProjectService],
+  providers: [
+    ProjectContextService,
+    ProjectOnboardingService,
+    ProjectService,
+  ],
+  exports: [ProjectContextService, ProjectService],
 })
 export class ProjectModule {}
