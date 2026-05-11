b55a2c7569a1d6f12815dc271cbac3a0b3bc9aec
HoangBao <vohoangbaoqn123@gmail.com>
2026-05-11 13:30:49 +0700

    .

--- src/modules/project/project.module.ts
+++ src/modules/project/project.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProjectMemberModule } from '@src/modules/project-member/project-member.module';
 import { UserModule } from '@src/modules/user/user.module';
+import { ProjectCommandHandler } from './project-command.handler';
+import { ProjectContextService } from './project-context.service';
 import { ProjectOnboardingService } from './project-onboarding.service';
 import { ProjectController } from './project.controller';
 import ProjectEntity from './project.entity';
-import { ProjectCommandHandler } from './project-command.handler';
-import { ProjectContextService } from './project-context.service';
 import { ProjectService } from './project.service';
 
 @Module({
