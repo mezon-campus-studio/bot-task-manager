aa190ffd01398b097380e84e8de6d48b5a37db48
HoangBao <vohoangbaoqn123@gmail.com>
2026-05-17 14:10:35 +0700

    fix bot review 1

--- src/database/migrations/1778992093596-AddUserRelationToTeamMember.ts
+++ src/database/migrations/1778992093596-AddUserRelationToTeamMember.ts
@@ -5,9 +5,6 @@ export class AddUserRelationToTeamMember1778992093596 implements MigrationInterf
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "teams" ADD CONSTRAINT "FK_10c8e335dc32010ef90abe65cec" FOREIGN KEY ("leader_id") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE NO ACTION`,
-    );
-    await queryRunner.query(
       `ALTER TABLE "team_members" ADD CONSTRAINT "FK_c2bf4967c8c2a6b845dadfbf3d4" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
     );
   }
@@ -15,9 +12,6 @@ export class AddUserRelationToTeamMember1778992093596 implements MigrationInterf
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       `ALTER TABLE "team_members" DROP CONSTRAINT "FK_c2bf4967c8c2a6b845dadfbf3d4"`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "teams" DROP CONSTRAINT "FK_10c8e335dc32010ef90abe65cec"`,
     );
   }
 }
