a7f7e3c1e72d408a5f594e13a3bb91d29e44d256
HoangBao <vohoangbaoqn123@gmail.com>
2026-06-07 10:12:34 +0700

    Revert "fix redis"
    This reverts commit df5bdc64070b086e86510bd91e4f9291250e000a.

--- test/jest/redis.ts
+++ test/jest/redis.ts
@@ -1,13 +1,20 @@
 import { env } from 'node:process';
+import { RedisMemoryServer } from 'redis-memory-server';
+import type { RedisInstanceDataT } from 'redis-memory-server/lib/RedisMemoryServer';
 
-export interface RedisConfig {
-  host: string;
-  port: number;
+export const NO_REDIS = env.NO_REDIS !== 'false';
+
+const REDIS = new RedisMemoryServer({
+  autoStart: false,
+  binary: {
+    version: '7.2.4',
+  },
+});
+
+export async function startRedis(): Promise<RedisInstanceDataT> {
+  return REDIS.start().then(() => REDIS.ensureInstance());
 }
 
-export function getRedisConfig(): RedisConfig {
-  return {
-    host: env.REDIS_HOST || 'localhost',
-    port: parseInt(env.REDIS_PORT || '6379', 10),
-  };
+export async function stopRedis(): Promise<boolean> {
+  return REDIS.stop();
 }
